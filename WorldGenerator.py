2c609f358872ab8266a12f933207958c4a4a9334
hlmoore6 <hlmoore6@mail.fhsu.edu>
2021-03-08 16:58:36 -0600

    Added structure to WorldGenerator

--- WorldGenerator.py
+++ WorldGenerator.py
@@ -20,13 +20,22 @@ class WorldGenderator:
         endingRoom = Room.Room.generateEndingRoom()
 
         rooms = list()
-
+        
+        #Initialize rooms layout
         for x in range(0, self.width):
             rooms.append(list())
             for y in range(0, self.height):
                 rooms[x].append(None)
+
+        #Rooms generation code
+
+        #Set starting room in the middle of the rooms list
+        rooms[self.width//2][self.height//2] = startingRoom
                 
         return 0
+
+    def generateLocalRooms(room):
+
 
 
 
